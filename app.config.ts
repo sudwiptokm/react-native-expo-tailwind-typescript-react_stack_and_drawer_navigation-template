a059ca3c36b7d4ebf241bdad39a2a59cdbdb4ba2
Sudwipto Mondal <sudwipto@Sudwiptos-MacBook-Pro.local>
2023-01-18 21:32:39 +0600

    Done

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+import { ExpoConfig } from "expo/config";
+
+// In SDK 46 and lower, use the following import instead:
+// import { ExpoConfig } from '@expo/config-types';
+
+const config: ExpoConfig = {
+  name: "my-app",
+  slug: "my-app",
+};
+
+export default config;
