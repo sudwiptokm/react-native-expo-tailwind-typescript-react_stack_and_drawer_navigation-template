50b8c8be247fe166f844f64a964a056d68adcb1e
Sudwipto Kumar Mondal <94325115+sudwiptokm@users.noreply.github.com>
2023-01-19 08:03:00 +0600

    without drawer

--- App.tsx
+++ App.tsx
@@ -1,32 +1,12 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
 import Root from "./src/screens/Root";
-import CustomDrawerContent from "./src/components/complex/CustomDrawerContent";
-import { Dimensions } from "react-native";
 
-const Drawer = createDrawerNavigator();
-
-export default function App() {
+export default function App()
+ {
   return (
     <NavigationContainer>
-      <Drawer.Navigator
-        screenOptions={{
-          drawerStyle: {
-            width: Dimensions.get("window").width * 0.7,
-          },
-        }}
-        useLegacyImplementation
-        drawerContent={() => <CustomDrawerContent />}
-      >
-        <Drawer.Screen
-          name="Root"
-          component={Root}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </Drawer.Navigator>
+      <Root />
     </NavigationContainer>
   );
 }
